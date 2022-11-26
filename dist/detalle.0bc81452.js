ad53d73726e03f613b664dcb7ada6125eba2c678
rai <rai@iMac-de-rai.local>
2022-11-26 01:01:24 +0100

    First Commit

--- dist/detalle.0bc81452.js
+++
@@ -1,2 +0,0 @@
-function inventos(){document.getElementById("inventosjs").style.color="#F0FF00",document.getElementById("inventosjs").innerHTML="Accede a la sección categorías",document.getElementById("inventosjs").style.fontWeight="900"}
-//# sourceMappingURL=detalle.0bc81452.js.map
